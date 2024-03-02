f97f1811d9e7423d8f92bc9250746f6a4f25b09e
yugmakkar <161882518+yugmakkar@users.noreply.github.com>
2024-03-02 13:03:47 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//Leaflet map
+var map = L.map('map').setView([28.668667, 77.10194], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
